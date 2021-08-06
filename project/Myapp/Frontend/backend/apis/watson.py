2b025dc3907d4f6b29c8b91c348014f897bc6b6a
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-08-06 15:03:49 -0300

    upgrade

--- project/Myapp/Frontend/backend/apis/watson.py
+++ project/Myapp/Frontend/backend/apis/watson.py
@@ -3,7 +3,7 @@
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
+from ibm_watson.natural_language_understanding_v1 import Features, RelationsOptions
 
 class apiWatson():
 
@@ -18,7 +18,7 @@ class apiWatson():
      natural_language_understanding.set_service_url(str(url.read()))
      response = natural_language_understanding.analyze(
         text='{0}'.format(texto),
-        features=Features(keywords=KeywordsOptions(limit=15))).get_result()
+        features=Features(relations=RelationsOptions(limit=15))).get_result()
      print(json.dumps(response, indent=2))
      
 
